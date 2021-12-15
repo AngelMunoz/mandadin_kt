e87402e2a1e8c57c5a036b52ce4bb85180b62e80
Angel D. Munoz <scythes.lol@gmail.com>
2021-12-15 11:52:21 -0600

    remove unused imports

--- app/src/main/java/me/tunaxor/apps/mandadin/MainActivity.kt
+++ app/src/main/java/me/tunaxor/apps/mandadin/MainActivity.kt
@@ -3,27 +3,13 @@ package me.tunaxor.apps.mandadin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import me.tunaxor.apps.mandadin.pages.NotesPage
 import me.tunaxor.apps.mandadin.ui.theme.MandadinTheme
-import kotlin.reflect.typeOf
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
